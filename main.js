aab94a8b7fc70f12f8adc1e1880a77cc7f95b414
ubgamesuk <148251552+ubgamesuk@users.noreply.github.com>
2024-11-23 16:07:21 +0000

    website update

--- main.js
+++ main.js
@@ -1,53 +1,55 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
 function myMenuFunction(){
-    var menuBtn = document.getElementById("myNavMenu");
+  var menuBtn = document.getElementById("myNavMenu");
 
-    if(menuBtn.className === "nav-menu"){
-      menuBtn.className += " responsive";
-    } else {
-      menuBtn.className = "nav-menu";
-    }
+  if(menuBtn.className === "nav-menu"){
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
   }
+}
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-  window.onscroll = function() {headerShadow()};
+window.onscroll = function() {headerShadow()};
 
-  function headerShadow() {
-    const navHeader =document.getElementById("header");
+function headerShadow() {
+  const navHeader =document.getElementById("header");
 
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
 
-      navHeader.style.height = "70px";
-      navHeader.style.lineHeight = "70px";
-      navHeader.style.borderBottom = "1px solid #a3a3a3"
+    navHeader.style.height = "60px";
+    navHeader.style.lineHeight = "40px";
+    navHeader.style.backgroundColor = "#fff";
+    navHeader.style.borderBottomLeftRadius = "15px";
+    navHeader.style.borderBottomRightRadius = "15px";
 
-    } else {
+  } else {
 
-      navHeader.style.boxShadow = "none";
-      navHeader.style.height = "90px";
-      navHeader.style.lineHeight = "90px";
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
 
-    }
   }
+}
 
 
 /* ----- TYPING EFFECT ----- */
- var typingEffect = new Typed(".typedText",{
-    strings : ["over 200 games","52,000 global users","over 10k 5★ reviews"],
-    loop : true,
-    typeSpeed : 100, 
-    backSpeed : 80,
-    backDelay : 2000
- })
+var typingEffect = new Typed(".typedText",{
+  strings : ["over 200 games","52,000 global users","over 10k 5★ reviews"],
+  loop : true,
+  typeSpeed : 100, 
+  backSpeed : 80,
+  backDelay : 2000
+})
 
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
- const sr = ScrollReveal({
-        origin: 'top',
-        distance: '80px',
-        duration: 2000,
-        reset: true     
- })
+const sr = ScrollReveal({
+      origin: 'top',
+      distance: '80px',
+      duration: 2000,
+      reset: true     
+})
 
 /* -- HOME -- */
 sr.reveal('.featured-text-card',{})
@@ -68,10 +70,10 @@ sr.reveal('.top-header',{})
 
 /* -- ABOUT INFO & CONTACT INFO -- */
 const srLeft = ScrollReveal({
-  origin: 'left',
-  distance: '80px',
-  duration: 2000,
-  reset: true
+origin: 'left',
+distance: '80px',
+duration: 2000,
+reset: true
 })
 
 srLeft.reveal('.about-info',{delay: 100})
@@ -79,10 +81,10 @@ srLeft.reveal('.contact-info',{delay: 100})
 
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
-  origin: 'right',
-  distance: '80px',
-  duration: 2000,
-  reset: true
+origin: 'right',
+distance: '80px',
+duration: 2000,
+reset: true
 })
 
 srRight.reveal('.skills-box',{delay: 100})
@@ -95,23 +97,23 @@ srRight.reveal('.form-control',{delay: 100})
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
-  const scrollY = window.scrollY;
+const scrollY = window.scrollY;
 
-  sections.forEach(current =>{
-    const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute('id')
+sections.forEach(current =>{
+  const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+    sectionId = current.getAttribute('id')
 
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+  if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
 
-        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
 
-    }  else {
+  }  else {
 
-      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
 
-    }
-  })
+  }
+})
 }
 
 window.addEventListener('scroll', scrollActive)
